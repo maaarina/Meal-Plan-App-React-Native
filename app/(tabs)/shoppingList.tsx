710396ec7c58872aa63aa3a7406788ad5d42f571
Marina Gabrielly <marina.gabri3lly@gmail.com>
2025-07-20 16:05:29 -0300

    test

--- app/(tabs)/shoppingList.tsx
+++ app/(tabs)/shoppingList.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function HomeScreen() {
+export default function ShoppingList() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Meal Plan</Text>
+      <Text>Shopping List</Text>
     </View>
   );
 }
