8edb1b9c8b9cff9a3ef2b7a6e396cffbbbc0055b
Marina Gabrielly <marina.gabri3lly@gmail.com>
2025-07-21 19:44:14 -0300

    update style

--- app/style.ts
+++ app/style.ts
@@ -2,22 +2,22 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
+        backgroundColor: "#FFF",
         margin: "auto",
         width: "100%",
         padding: 25,
         alignSelf: "flex-start",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#FFF",
     },
     dayTitle: {
         backgroundColor: "#FFACAC",
+        fontSize: 20,
+        fontWeight: "bold",
         marginTop: 20,
         padding: 6,
         alignSelf: "flex-start",
         borderRadius: 5,
         marginBottom: 7,
-        fontWeight: "bold",
-        fontSize: 20,
   },
 })
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -2,12 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const styleHeader = StyleSheet.create({
   container: {
-    padding: 16,
     backgroundColor: "#FFF",
     alignItems: "center",
   },
   titulo: {
-    paddingTop: 15,
+    paddingTop: 30,
     fontSize: 24,
     fontWeight: "bold",
     color: "#333",
